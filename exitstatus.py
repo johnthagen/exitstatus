a3df243e148e2a208e948a56aae629bac338bf8e
johnthagen <johnthagen@gmail.com>
2016-10-11 18:26:12 -0400

    Add docstrings.  Fixes #2.

--- exitstatus.py
+++ exitstatus.py
@@ -1,6 +1,13 @@
+"""Provides portable POSIX exit codes in the form of an IntEnum."""
+
 import enum
 
 
 class ExitStatus(enum.IntEnum):
+    """Portable definitions for the standard POSIX exit codes."""
+
     success = 0
+    """Indicates successful program completion."""
+
     failure = 1
+    """Indicates unsuccessful program completion in a general sense."""
